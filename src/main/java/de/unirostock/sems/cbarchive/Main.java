edc5dbbcdfeebb5d3c6a9c1aaa1f0611d12adef9
Martin Scharm <martin@binfalse.de>
2014-05-26 00:10:02 +0200

    started main file [see #9]

---
+++ src/main/java/de/unirostock/sems/cbarchive/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package de.unirostock.sems.cbarchive;
+
+
+/**
+ * @author Martin Scharm
+ *
+ */
+public class Main
+{
+	
+	/**
+	 * @param args
+	 */
+	public static void main (String[] args)
+	{
+		System.out.println ("to be developed...");
+		System.exit (2);
+	}
+	
+}
